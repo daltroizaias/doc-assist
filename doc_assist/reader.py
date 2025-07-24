859953e89913cf7c98f15e293f006e9b02d1ab70
daltroizaias <daltroizaias@gmail.com>
2025-07-24 17:08:18 -0300

    organizando o arquivo de leitura e transformação para markdown

--- doc_assist/reader.py
+++ doc_assist/reader.py
@@ -3,13 +3,17 @@ from docling.document_converter import DocumentConverter
 from settings import settings
 import os
 from tqdm import tqdm
-# %%
-ROOT_PDF = settings.INPUT_FILE
+
+
+
 ROOT_MD = settings.OUTPUT_FILE
+ROOT_PDF = settings.INPUT_FILE
 
-
+# %%
 def load_save_file(file: str) -> str:
-
+    """
+    Carrega os dados PDF e os tranforma em Markdown e os salva na pasta
+    """
     print('convertendo')
     converter = DocumentConverter()
     result = converter.convert(file)
@@ -23,13 +27,32 @@ def load_save_file(file: str) -> str:
 
     return result
 
+def verificador(pasta_pdf: str, pasta_markdown: str):
+    """
+    Verifica quais os arquivos já foram processados e mantem somente
+    aqueles que ainda não foram processados
+    """
 
-# %% 
 
-files = os.listdir(ROOT_PDF)
-progress_bar = tqdm(files)
-for file in progress_bar:
-    progress_bar.set_description(f'Processando arquivo {file.split('/')[-1]}')
+    files_markdown = os.listdir(pasta_markdown)
+    files_markdown = [file.replace('.md', '') for file in files_markdown]
 
-    load_save_file(file=os.path.join(ROOT_PDF, file))
-# %%
+    files_pdf = os.listdir(pasta_pdf)
+    file_unique = []
+    for file in files_pdf:
+        if file not in files_markdown:
+           file_unique.append(file)
+
+    return file_unique
+
+def run():
+    files = verificador(ROOT_PDF, ROOT_MD)
+    progress_bar = tqdm(files)
+    for file in progress_bar:
+        progress_bar.set_description(f'Processando arquivo {file.split('/')[-1]}')
+
+        load_save_file(file=os.path.join(ROOT_PDF, file))
+
+
+if __name__ == '__main__':
+    run()
